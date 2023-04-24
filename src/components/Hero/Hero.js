8b0df1f17e5478bef2422b5029314ae965402cb4
johnny <jbhall4291@gmail.com>
2023-04-24 18:01:26 +0100

    style Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,36 +6,26 @@ export const Hero = () => {
   return (
     <>
       <section id="hero" className="Hero">
-        <div className="Hero--image">
+        <div className="Hero__div--image-container">
           <img src={profilePic} alt="johnny"></img>
         </div>
-        <div className="Hero--text">
+
+        <div className="Hero__div--text-container">
           <h1>Hi, I'm Johnny</h1>
 
-          <h2 className="Hero__h2">
-            <span className="Hero__span fade-in-underline">
-              Full-stack software developer
-            </span>{" "}
-            by day, pizza enthusiast by night 🍕 <br></br> I love{" "}
-            <span className="Hero__span fade-in-underline">
-              building cool stuff
-            </span>{" "}
-            and{" "}
-            <span className="Hero__span fade-in-underline">
-              learning new technologies{" "}
-            </span>
-            🎓 <br></br> Whether it's front-end or back-end, I'm always{" "}
-            <span className="Hero__span fade-in-underline">
-              up for a challenge
-            </span>{" "}
-            🚀
-          </h2>
+          <p>
+            <span>Full-stack software developer</span> by day, pizza enthusiast
+            by night 🍕 I love <span>building cool stuff</span> and
+            <span> learning new technologies </span>
+            🎓 Whether it's front-end or back-end, I'm always
+            <span> up for a challenge</span> 🚀
+          </p>
 
-          <h2 className="Hero__h2">
+          <p>
             I'm currently looking for an opportunity to contribute to a
             company's success, so if you're looking for a junior developer then
             get in touch & let's make something amazing!
-          </h2>
+          </p>
 
           <div className="Hero__div--socials-container">
             <a
@@ -61,7 +51,6 @@ export const Hero = () => {
         </div>
       </section>
       <div className="Hero__div--angled"></div>
-      {/* <div className="triangle"></div> */}
     </>
   );
 };
