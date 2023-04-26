6c2fc5ad81cf824742aecaea0d1ed4e545116353
johnny <jbhall4291@gmail.com>
2023-04-26 22:00:09 +0100

    styling for mobile

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,7 +8,9 @@ export const Hero = () => {
       <section id="hero" className="Hero">
         <div className="Hero__div--container">
           <div className="Hero__div--image-container">
+          
             <img src={profilePic} alt="johnny"></img>
+            
           </div>
 
           <div className="Hero__div--text-container">
