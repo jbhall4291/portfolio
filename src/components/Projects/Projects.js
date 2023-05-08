4bf6718176ae8b68907fa8a9f1564e8a45eb7c9b
johnny <jbhall4291@gmail.com>
2023-05-08 21:57:35 +0100

    update urls

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -16,15 +16,14 @@ export const Projects = () => {
           <img src={projectPlaceholder} alt="johnny"></img>
         </div>
         <div className="Projects__div--title-area">
-          <h3>Northcoders News</h3>
-          <h4>reddit-style news aggregation web app</h4>
+          <h3>NewsBuzz</h3>
+          <h4>a reddit-style social news aggregation web app</h4>
         </div>
         <div className="Projects__div--desc-area">
           <p className="Projects__p--description">
-            Northcoders News aka Buzzboard aka AggreGator 🐊 is a news
-            aggregation React app in the style of Reddit, allowing users to
-            view, vote and comment on articles hosted on my own backend and psql
-            database. This was built as part of the amazing Northcoders
+            NewsBuzz is a React web app in the style of Reddit, allowing users
+            to view, vote and comment on articles hosted on my own backend and
+            psql database. This was built as part of the amazing Northcoders
             Full-Stack Software Developer Bootcamp, with the aim of bringing
             together my front and back end skills under one project!
           </p>
@@ -51,10 +50,13 @@ export const Projects = () => {
           </div>
 
           <div className="Projects__div--button-container">
-            <ButtonFill text="live app" href="https://www.google.com" />
+            <ButtonFill
+              text="live app"
+              href="https://newsbuzz-jbhall4291.netlify.app/"
+            />
             <ButtonFill
               text="github repo"
-              href="https://github.com/jbhall4291/nc-news-frontend"
+              href="https://github.com/jbhall4291/newsbuzz"
             />
             <ButtonFill text="video demo" href="https://www.google.com" />
           </div>
@@ -66,17 +68,17 @@ export const Projects = () => {
           <img src={fanfinderIpad} alt="johnny"></img>
         </div>
         <div className="Projects__div--title-area">
-          <h3>FanFinder</h3>
-          <h4>location-based social networking mobile app </h4>
+          <h3>Fan Finder</h3>
+          <h4>a location-based social networking mobile app </h4>
         </div>
         <div className="Projects__div--desc-area">
           <p className="Projects__p--description">
-            FanFinder is a mobile app built in React Native and allows music
+            Fan Finder is a mobile app built in React Native and allows music
             fans to discover upcoming gigs in their area, and connect with other
             music fans via gig-specific forums or a chat facility. This project
             was built by a team of 4 using the MERN stack as part of the final
             Northcoders Bootcamp where we had to pitch, plan, build and deploy a
-            project, all in just under 2 weeks!
+            project, all in just under two weeks!
           </p>
 
           <div className="Projects__div--tech-container">
@@ -124,7 +126,7 @@ export const Projects = () => {
         </div>
         <div className="Projects__div--title-area">
           <h3>One Hundred Press-ups</h3>
-          <h4>fitness tracking web & mobile app</h4>
+          <h4>a fitness tracking web & mobile app</h4>
           <h4>
             <em>~ under development ~</em>
           </h4>
