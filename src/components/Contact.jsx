7889035d3f61c3211aa6bd5c0ab760bbfeeec666
johnny <jbhall4291@gmail.com>
2023-06-03 17:02:29 +0100

    refine style & layout

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,110 +17,109 @@ function Contact() {
   return (
     <section
       id="contact"
-      className=" container mx-auto flex flex-col px-5 py-10 text-center lg:px-10"
+      className=" container mx-auto flex flex-col  px-5 py-10 text-center lg:px-10"
     >
       <Typography color="blue-gray" className=" text-center lg:text-lg">
         If you would like a copy of my CV or are interested in collaborating on
-        a project I’d love to hear from you!
+        a project, then I’d love to hear from you!
       </Typography>
 
-      <div className="flex flex-wrap justify-center gap-1.5 py-5 md:m-auto md:w-5/6 md:gap-9 md:py-10 ">
-        <a
-          href="mailto:jbhall4291@gmail.com"
-          className="inline-block "
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Button
-            size="lg"
-            variant="text"
-            className="flex items-center gap-2 bg-highlight align-middle text-white hover:bg-highlight  hover:shadow-xl"
+      <div className="mx-auto flex w-11/12 flex-col items-center justify-center gap-5 py-5 sm:w-3/4 md:flex-row">
+        <div className="flex flex-wrap items-center justify-center gap-1.5  pb-5 md:order-2 md:m-auto md:flex-col md:gap-9 ">
+          <a
+            href="mailto:jbhall4291@gmail.com"
+            target="_blank"
+            rel="noreferrer"
           >
-            EMAIL ME
-            <i className="fa-solid fa-envelope fa-xl"></i>
-          </Button>
-        </a>
+            <Button
+              size="lg"
+              variant="text"
+              className="flex items-center gap-2 bg-highlight align-middle text-white hover:bg-highlight  hover:shadow-xl"
+            >
+              EMAIL ME
+              <i className="fa-solid fa-envelope fa-xl"></i>
+            </Button>
+          </a>
 
-        <a
-          href="https://github.com/jbhall4291"
-          className="inline-block "
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Button
-            size="lg"
-            variant="text"
-            className="flex items-center gap-2 bg-highlight align-middle text-white hover:bg-highlight  hover:shadow-xl"
+          <a
+            href="https://www.linkedin.com/in/johnny-hall-dev"
+            target="_blank"
+            rel="noreferrer"
           >
-            VIEW MY GITHUB
-            <i className="fa-brands fa-github fa-xl"></i>
-          </Button>
-        </a>
+            <Button
+              size="lg"
+              variant="text"
+              className="flex items-center gap-2 bg-highlight align-middle text-white hover:bg-highlight  hover:shadow-xl"
+            >
+              CONNECT ON LINKEDIN
+              <i className="fa-brands fa-linkedin fa-xl"></i>
+            </Button>
+          </a>
 
-        <a
-          href="https://www.linkedin.com/in/johnny-hall-dev"
-          className="inline-block "
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Button
-            size="lg"
-            variant="text"
-            className="flex items-center gap-2 bg-highlight align-middle text-white hover:bg-highlight  hover:shadow-xl"
+          <a
+            href="https://github.com/jbhall4291"
+            target="_blank"
+            rel="noreferrer"
           >
-            CONNECT ON LINKEDIN
-            <i className="fa-brands fa-linkedin fa-xl"></i>
-          </Button>
-        </a>
-      </div>
+            <Button
+              size="lg"
+              variant="text"
+              className="flex items-center gap-2 bg-highlight align-middle text-white hover:bg-highlight  hover:shadow-xl"
+            >
+              VIEW MY GITHUB
+              <i className="fa-brands fa-github fa-xl"></i>
+            </Button>
+          </a>
+        </div>
 
-      <form
-        name="contact"
-        method="post"
-        className="mx-auto flex w-80 flex-col rounded p-10 text-left text-blueGrey shadow-xl"
-      >
-        <input type="hidden" name="form-name" value="contact" />
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          required
-          className="mb-4  box-border w-full rounded-md border-2 border-solid border-blueGrey p-1 text-lg"
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          required
-          className="mb-4   box-border w-full rounded-md border-2 border-solid border-blueGrey p-1 text-lg"
-        />
-        <label htmlFor="message">Message</label>
-        <textarea
-          id="message"
-          name="message"
-          rows="6"
-          required
-          className=" mb-4 box-border  w-full resize-none rounded-md border-2 border-solid border-blueGrey p-1 text-lg"
-        ></textarea>
-        <div className="flex">
-          {/* <input
+        <form
+          name="contact"
+          method="post"
+          className="mx-auto flex w-full p-5   flex-col rounded  text-left text-blueGrey shadow-xl sm:w-96  md:p-10"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            required
+            className="mb-4  box-border w-full rounded-md border-2 border-solid border-blueGrey p-1 text-lg"
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            className="mb-4   box-border w-full rounded-md border-2 border-solid border-blueGrey p-1 text-lg"
+          />
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            rows="6"
+            required
+            className=" mb-4 box-border  w-full resize-none rounded-md border-2 border-solid border-blueGrey p-1 text-lg"
+          ></textarea>
+          <div className="flex">
+            {/* <input
             value="Submit Message"
             className="mx-auto inline-flex rounded border-0 bg-highlight px-6 py-2 text-lg text-white hover:bg-green-600 focus:outline-none md:text-center"
           /> */}
 
-          <Button
-            type="submit"
-            size="lg"
-            variant="text"
-            value="Submit Message"
-            className="mx-auto flex w-full items-center justify-center gap-2 bg-highlight text-white hover:bg-highlight  hover:shadow-xl"
-          >
-            SUBMIT MESSAGE
-          </Button>
-        </div>
-      </form>
+            <Button
+              type="submit"
+              size="lg"
+              variant="text"
+              value="Submit Message"
+              className="mx-auto flex w-full items-center justify-center gap-2 bg-highlight text-white hover:bg-highlight  hover:shadow-xl"
+            >
+              SUBMIT MESSAGE
+            </Button>
+          </div>
+        </form>
+      </div>
     </section>
   );
 }
