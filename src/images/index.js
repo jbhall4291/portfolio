d8291c90b01d4cb4e44ead47e9a38b7f02b6c29e
johnny <jbhall4291@gmail.com>
2023-06-11 00:17:46 +0100

    optimise image size & change to jpg format

--- src/images/index.js
+++ src/images/index.js
@@ -2,12 +2,12 @@ import profile from "./profile.jpg";
 import cab from "./cab.jpeg";
 import ivy from "./ivy.jpeg";
 import sydney from "./sydney.jpeg";
-import fanfinder from "./fanfinder.png";
-import newsbuzz from "./newsbuzz.png";
-import newsbuzzCard from "./newsbuzz-card.png";
-import fanfinderCard from "./fanfinder-card.png";
+import fanfinder from "./fanfinder.jpg";
+import newsbuzz from "./newsbuzz.jpg";
+import newsbuzzCard from "./newsbuzz-card.jpg";
+import fanfinderCard from "./fanfinder-card.jpg";
 import oneHundredPressUpsCard from "./one-hundred-pressups-card.jpg";
-import oneHundredPressUps from "./one-hundred-pressups.png";
+import oneHundredPressUps from "./one-hundred-pressups.jpg";
 
 export {
   cab,
