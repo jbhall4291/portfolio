e865bf9c19bc78e053b33fc12e6287eb1208d5b1
johnny <jbhall4291@gmail.com>
2023-06-14 18:02:01 +0100

    styling tweaks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const currentYear = new Date().getFullYear();
 
 function Footer() {
   return (
-    <footer className=" text-md mt-10 bg-white py-2 text-center text-myTheme sm:pr-2 sm:text-right md:px-6">
+    <footer className=" text-md mt-16 bg-white py-2 text-center text-myTheme sm:pr-2 sm:text-right md:px-6">
       Designed & built with 🍕 by Johnny Hall © {currentYear}
     </footer>
   );
